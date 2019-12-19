abedee0ea5635af193acf2f13287fea28360343d
Rahulthiru125 <rahulthiru125@gmail.com>
2019-12-19 02:11:41 -0800

    entering data

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const port =process.env.PORT||3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/index.html'))
 
+var obj={}
+obj.name="Beingzero";
+obj.college="CMRCET";
+obj.regno="12345678";
+app.get('/data', (req, res) => res.json(obj))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
